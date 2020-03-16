1ef209d3723365cef02957001b656a86ce34db12
gc89925 <59216306+gc89925@users.noreply.github.com>
2020-03-16 18:57:48 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,8 +2,8 @@
 #=================================================
 # Description: DIY script
 # Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
+# Author: GAO SIR
+# Blog: https://1.gc89925.xyz
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.123.99/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate

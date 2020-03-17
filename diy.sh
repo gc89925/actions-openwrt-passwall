e34380eaf9fa42d0d05c9df323069765d8864c1c
gc89925 <59216306+gc89925@users.noreply.github.com>
2020-03-17 22:05:20 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: GAO SIR
-# Blog: https://1.gc89925.xyz
+=================================================
+ Description: DIY script
+ Lisence: MIT
+ Author: GAO SIR
+ Blog: https://1.gc89925.xyz
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate

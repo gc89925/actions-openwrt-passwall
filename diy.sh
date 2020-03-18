c02ed0deb28d91a9a1c521999eca29160272437b
gc89925 <59216306+gc89925@users.noreply.github.com>
2020-03-18 20:35:47 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,6 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+git clone https://github.com/Lienol/openwrt-package
+./scripts/feeds update -a
+./scripts/feeds install -a

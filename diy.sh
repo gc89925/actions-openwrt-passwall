d75d8e41776f09ac640da0b2910825e4c38d6f4d
gc89925 <59216306+gc89925@users.noreply.github.com>
2020-03-22 05:20:48 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,5 +10,6 @@ sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generat
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
 git clone https://github.com/Lienol/openwrt-package
 git clone -b dev-lean-lede https://github.com/Lienol/openwrt lean-lede
+git clone -b master --single-branch https://github.com/LGA1150/openwrt-fullconenat package/fullconenat
 ./scripts/feeds update -a
 ./scripts/feeds install -a

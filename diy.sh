a30c19948ec58f4df2e0eacc2c24c799fe633d95
gc89925 <59216306+gc89925@users.noreply.github.com>
2020-03-22 01:05:29 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,7 +6,8 @@
  Blog: https://1.gc89925.xyz
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate	
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
 git clone https://github.com/Lienol/openwrt-package
 git clone -b dev-lean-lede https://github.com/Lienol/openwrt lean-lede
 ./scripts/feeds update -a
